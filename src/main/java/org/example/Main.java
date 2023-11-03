64b2a28d7d68719392c85c28b0f4f82b0bfe7641
hcs <hannahemcs@gmail.com>
2023-11-03 10:00:34 +0100

    Create main method for testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserManagement userManagement = new UserManagement();
+
+        userManagement.registerUser("hannah", "helloworld", UserRole.STUDENT);
+        userManagement.registerUser("faruk", "ilovejava", UserRole.TEACHER);
+        userManagement.registerUser("mozamel", "startsteps", UserRole.ADMIN);
+
+        //successful login
+        System.out.println(userManagement.loginUser("hannah", "helloworld", UserRole.STUDENT));
+        //unsuccessful login
+        System.out.println(userManagement.loginUser("faruk", "helloworld", UserRole.TEACHER));
     }
 }
